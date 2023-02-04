a22616f356d436e1312d5a159570ca7f8dfcffdd
babik <babi.kolli@gmail.com>
2023-02-05 00:03:18 +0530

    code edited

--- create-user.sh
+++ create-user.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-
 # This script will create a linux user
+
+USER_NAME=$1
+COMMENT=$2
+PASSWORD=$3
 
 # Ask for the user name.
 read -p 'Enter the username to create: ' USER_NAME
+
+# Ask for the real Name.
+read -p 'Enter the name of the person who this account is for: ' COMMENT
 
 # Ask for the Password.
 read -p 'Enter the password to use for the account: ' PASSWORD
 
 # Create the user.
-useradd -c "${COMMENT}" -m ${USER_NAME}
+sudo useradd -c "${COMMENT}" -m ${USER_NAME}
 
 # Set the password for the user.
-echo "${USER_NAME}:${PASSWORD}" | chpasswd
+sudo echo "${USER_NAME}:${PASSWORD}" | chpasswd
 
 #Force Password change on first login.
-passwd -e ${USER_NAME}
+sudo passwd -e ${USER_NAME}
 
-# show all the users in server
+# shows the user created in server
 sudo getent passwd | grep $USER_NAME
